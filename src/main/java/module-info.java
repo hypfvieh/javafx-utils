8fcaecaa1cf7beb9af9469889bd80b1b17ffafdd
David M <maniac@case-of.org>
2021-11-18 08:22:04 +0100

    Renamed module; Fixed exports vs opens; Updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,24 @@
-module hypfvieh.javafx.utils {
-    exports com.github.hypfvieh.javafx.beans;
-    exports com.github.hypfvieh.javafx.factories;
-    exports com.github.hypfvieh.javafx.app;
-    exports com.github.hypfvieh.javafx.interfaces;
-    exports com.github.hypfvieh.javafx.controls.listview;
-    exports com.github.hypfvieh.javafx.windowsaver;
-    exports com.github.hypfvieh.javafx.db;
-    exports com.github.hypfvieh.javafx.controls;
-    exports com.github.hypfvieh.javafx.fx;
-    exports com.github.hypfvieh.javafx.other;
-    exports com.github.hypfvieh.javafx.ui;
-    exports com.github.hypfvieh.javafx.controls.table;
-    exports com.github.hypfvieh.javafx.formatter;
-    exports com.github.hypfvieh.javafx.functional;
-    exports com.github.hypfvieh.javafx.utils;
-    exports com.github.hypfvieh.javafx.windows.interfaces;
-    exports com.github.hypfvieh.javafx.converter;
-    exports com.github.hypfvieh.javafx.generic;
-    exports com.github.hypfvieh.javafx.dialogs;
-    exports com.github.hypfvieh.javafx.fx.fonts;
+module com.github.hypfvieh.javafx.utils {
+    opens com.github.hypfvieh.javafx.beans;
+    opens com.github.hypfvieh.javafx.factories;
+    opens com.github.hypfvieh.javafx.app;
+    opens com.github.hypfvieh.javafx.interfaces;
+    opens com.github.hypfvieh.javafx.controls.listview;
+    opens com.github.hypfvieh.javafx.windowsaver;
+    opens com.github.hypfvieh.javafx.db;
+    opens com.github.hypfvieh.javafx.controls;
+    opens com.github.hypfvieh.javafx.fx;
+    opens com.github.hypfvieh.javafx.other;
+    opens com.github.hypfvieh.javafx.ui;
+    opens com.github.hypfvieh.javafx.controls.table;
+    opens com.github.hypfvieh.javafx.formatter;
+    opens com.github.hypfvieh.javafx.functional;
+    opens com.github.hypfvieh.javafx.utils;
+    opens com.github.hypfvieh.javafx.windows.interfaces;
+    opens com.github.hypfvieh.javafx.converter;
+    opens com.github.hypfvieh.javafx.generic;
+    opens com.github.hypfvieh.javafx.dialogs;
+    opens com.github.hypfvieh.javafx.fx.fonts;
 
     requires java.desktop;
     requires java.naming;
